474cb9cd7d27c8a4f73512cf8e043519b491e2d0
yixiu <aken@aken.com>
2017-06-14 19:00:39 +0800

    jquery 101-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get "/jquery-1" => "page#jquery_1"
-  get "/jquery-2" => "page#jquery_2"
-  get "/jquery-3" => "page#jquery_3"
-  get "/jquery-4" => "page#jquery_4"
-  get "/jquery-5" => "page#jquery_4"
+  get "/jquery-1" => "pages#jquery_1"
+  get "/jquery-2" => "pages#jquery_2"
+  get "/jquery-3" => "pages#jquery_3"
+  get "/jquery-4" => "pages#jquery_4"
+  get "/jquery-5" => "pages#jquery_4"
 
   root "pages#jquery_1"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
