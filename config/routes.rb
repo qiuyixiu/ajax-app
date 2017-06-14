f23f961af24599725047a1a172dd14c6987baef1
yixiu <aken@aken.com>
2017-06-14 17:16:39 +0800

    add bootstrap to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get "/jquery-1" => "page#jquery_1"
+  get "/jquery-2" => "page#jquery_2"
+  get "/jquery-3" => "page#jquery_3"
+  get "/jquery-4" => "page#jquery_4"
+  get "/jquery-5" => "page#jquery_4"
+
+  root "pages#jquery_1"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
